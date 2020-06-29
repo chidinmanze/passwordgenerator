4299b52b6538252cba2c798406529eb62c378d3b
Chidinma Nze <chidinma.ogbuaku@gmail.com>
2020-06-29 09:19:11 -0400

    adding working files

--- script.js
+++ script.js
@@ -1,76 +1,185 @@
 // Assignment Code
-var generateBtn = document.querySelector("generate");
-document.getElementById("generate").addEventListener("click", first);
 
+var specialCharacters = [
+  '!',
+  '"',
+  '#',
+  '$',
+  '%',
+  '&',
+  '(',
+  ')',
+  '*',
+  '+',
+  '-',
+  "'",
+  '.',
+  '/',
+  ':',
+  ';',
+  '<',
+  '=',
+  '>',
+  '?',
+  '@',
+  '[',
+  '\\',
+  ']',
+  '^',
+  '_',
+  '`',
+  '{',
+  '|',
+  '}',
+  '~'
+]
+var upperCaseCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+]
+var lowerCaseCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+]
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 // Password requirement questions (length, character types)
 
 // Length 8 - 128 characters
-function first () {
-var length = prompt("How long do you want your password to be? Choose a length between 8 and 128.");
-alert("You said"  +  length);
+function writePassword() {
+  
+  // var passwordText ={function generatePassword () {
+  var passwordLength = parseInt(prompt("How long do you want your password to be? Choose a length between 8 and 128."));
+  console.log("You said"  +  passwordLength);
+  console.log(typeof passwordLength)
+  
+  if (isNaN(passwordLength)){
+    return alert("password length must be a number");
+  }
+  if (passwordLength < 8 && passwordLength > 128 ){
+    return alert("Password must be between 8 and 128 characters long.");
+  }
+  // Lowercase characters (yes or no)
+  var lowerc = confirm("Include lowercase?");
+  if (lowerc === true) {
+    console.log("You said yes to lowercase");
+  }
+  else {
+    console.log("You said no to lowercase");
+  }
+  
+  // Uppercase characters (yes or no)
+  var upperc = confirm("Include uppercase?");
+  
+  if (upperc === true) {
+    console.log("You said yes to uppercase");
+  }
+  else {
+    console.log("You said no to uppercase");
+  }
+  
+  // Numbers (yes or no)
+  var numbers = confirm("Include numbers?");
+  
+  if (numbers === true){
+    console.log("You said yes to numbers");
+  }
+  else {
+    console.log("You said no to numbers");
+  }
+  
+  // Special characters (yes or no)
+  var special = confirm("Include special characters?");
+  
+  if (special === true) {
+    console.log("You said yes to special characters");
+  }
+  else {
+    console.log("You said no to special characters");
+  }
+  
+  console.log(passwordLength)
+  console.log(lowerc)
+  console.log(upperc)
+  console.log(numbers)
+  console.log(special)
+  
+  // conditional to create array of posssible characters
+  var passwordChoice = [];
+  if (lowerc === true) {passwordChoice = passwordChoice.concat(lowerCaseCharacters);
+    
+  }
+  if (upperc === true) {passwordChoice = passwordChoice.concat(upperCaseCharacters);
+    
+  }
+  if (numbers === true) {passwordChoice = passwordChoice.concat(numericCharacters);
+    
+  }
+  if (special === true) {passwordChoice = passwordChoice.concat(specialCharacters);
+  } 
+    
+    // loop to go through array until password length is met
+    
+    
+      var passwordString=""
+    for (var i = 0; i < passwordLength; i++) {
+      passwordString = passwordString + passwordChoice[Math.floor(Math.random() * passwordChoice.length)];
+    }
+    passwordText.textContent=passwordString;
+  }
+   
+  
 
-// Lowercase characters (yes or no)
-var lowerc = confirm("Include lowercase?");
-if (lowerc == true) {alert("You said yes to lowercase");
-}
-else if (lowerc == false) {alert("You said no to lowercase");
-}
-
-// Uppercase characters (yes or no)
-var upperc = confirm("Include uppercase?");
-if (upperc == true) {alert("You said yes to uppercase");
-}
-else if (upperc == false) {alert("You said no to uppercase");
-}
-
-// Numbers (yes or no)
-var numbers = confirm("Include numbers?");
-if (numbers == true) {alert("You said yes to numbers");
-}
-else if (numbers == false) {alert("You said no to numbers");
-}
-
-// Special characters (yes or no)
-var special = confirm("Include special characters?");
-if (special == true) {alert("You said yes to special characters");
-}
-else if (special == false) {alert ("You said no to special characters");
-}
-
-console.log(length)
-console.log(lowerc)
-console.log(upperc)
-console.log(numbers)
-console.log(special)
-
-}
-
-// List all possible variables
-var lowerctext = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var upperctext = ['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var numberstext = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var specialtext = ['~', '`', '!', '@', '#', '$', '%', '^', '*', '_', '-', '+', '<', '>', '?', '/', '|', ',', '.']
-
-function second (x) {
-  if (lowerc == true) lowerctext.join(x)};
-  if (upperc == true) upperctext.join(x);
-  if (numbers == true) numberstext.join(x);
-  if (special == true) specialtext.join(x);
-
-}
-
-
-function third () {
-  var password = x(Math.floor(Math.random)*length);
- }
-
-
-
-
-// Write password to the #password input
-function writepassString() {
- third.value = password
- document.querySelector("password").innerHTML = (password);
-
-}
+           
+      var generateBtn = document.querySelector("#generate");
+      var passwordText = document.querySelector("#password");
+   
+    generateBtn.addEventListener("click", writePassword);
